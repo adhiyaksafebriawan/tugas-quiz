36c3d7654dff8834a314daf1a3f5f7c21aadea48
adhiyaksafebriawan <adhiyaksafebriawan@gmail.com>
2024-10-31 12:42:28 +0700

    Create andikaepeda.cpp

---
+++ andikaepeda.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void hitungEK(double massa, double kecepatan) 
+{
+	double EK =0.5 * massa  * pow(kecepatan, 2);
+	cout << "EK : " << EK;
+}
+
+int main()
+{
+	double massa;
+	double kecepatan;
+	
+	cout << "massa : "; cin >> massa;
+	cout << "kecepatan : "; cin >> kecepatan;
+	
+	hitungEK(massa, kecepatan);
+	
+}
